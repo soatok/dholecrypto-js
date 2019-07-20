2754329b0007149c4765d42bbf6fad9080e9d887
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-20 16:08:28 -0400

    Expose more classes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
     'AsymmetricPublicKey': require('./lib/key/AsymmetricPublicKey'),
     'CryptoError': require('./lib/error/CryptoError'),
     'DholeUtil': require('./lib/Util'),
+    'Keyring': require('./lib/Keyring'),
+    'Password': require('./lib/Password'),
     'Symmetric': require('./lib/Symmetric'),
     'SymmetricKey': require('./lib/key/SymmetricKey')
 };
