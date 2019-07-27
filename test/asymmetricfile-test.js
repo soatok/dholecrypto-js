fda3235b0b7f46b18b8c7826e62838ce08b188f6
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-27 15:58:41 -0400

    Unit tests from generated test vectors (from PHP version)

--- test/asymmetricfile-test.js
+++ test/asymmetricfile-test.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const fsp = fs.promises;
 const AsymmetricFile = require('../lib/AsymmetricFile');
 const AsymmetricSecretKey = require('../lib/key/AsymmetricSecretKey');
+const Keyring = require('../lib/Keyring');
 const Util = require('../lib/Util');
+const loadJsonFile = require('load-json-file');
 
 describe('AsymmetricFile', function() {
     it('sign()', async function() {
@@ -18,5 +20,26 @@ describe('AsymmetricFile', function() {
         assert(await AsymmetricFile.verify(fh, alicePk, sig), 'Signatures not valid');
         await fh.close();
         await fsp.unlink(__dirname + "/signtest1.txt");
+    });
+
+    it('should pass the standard test vectors', async function() {
+        let json = await loadJsonFile('./test/test-vectors.json');
+        let keyring = new Keyring();
+        let publicKey = keyring.loadAsymmetricPublicKey(
+            json['asymmetric-file-sign']['public-key']
+        );
+        let i = 2;
+        let fh, filename;
+        for (let test of json['asymmetric-file-sign'].tests) {
+            filename = __dirname + "/signtest" + i + ".txt";
+            await fsp.writeFile(filename, test.contents);
+            fh = await fsp.open(filename, 'r');
+            assert(
+                await AsymmetricFile.verify(fh, publicKey, test.signature),
+                'Signatures not valid'
+            );
+            await fh.close();
+            await fsp.unlink(filename);
+        }
     });
 });
