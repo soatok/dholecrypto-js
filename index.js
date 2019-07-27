0acc9f039053b94837da1318c1bf4458cabe3d36
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-27 16:01:56 -0400

    Expose new APIs. Version 0.5.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = {
     'Asymmetric': require('./lib/Asymmetric'),
+    'AsymmetricFile': require('./lib/AsymmetricFile'),
     'AsymmetricSecretKey': require('./lib/key/AsymmetricSecretKey'),
     'AsymmetricPublicKey': require('./lib/key/AsymmetricPublicKey'),
     'CryptoError': require('./lib/error/CryptoError'),
@@ -8,6 +9,7 @@ module.exports = {
     'Keyring': require('./lib/Keyring'),
     'Password': require('./lib/Password'),
     'Symmetric': require('./lib/Symmetric'),
+    'SymmetricFile': require('./lib/SymmetricFile'),
     'SymmetricKey': require('./lib/key/SymmetricKey'),
     "_sodium": require('sodium-native')
 };
